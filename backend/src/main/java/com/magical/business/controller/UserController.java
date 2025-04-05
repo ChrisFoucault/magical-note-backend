230fbcb0fce07ec1f42f8f646b7cf94640d90477
chenshixuan <1193755730@qq.com>
2025-04-05 20:12:09 +0800

    初始化项目

---
+++ backend/src/main/java/com/magical/business/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.magical.business.controller;
+import com.magical.business.entity.User;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+@Tag(name = "用户管理", description = "提供用户的增删改查 API")
+public class UserController {
+
+    @PostMapping("/login")
+    public String login(User user) {
+        return "success";
+    }
+
+}
---
+++ backend/src/main/java/com/magical/business/entity/User.java
@@ -0,0 +1,9 @@
+package com.magical.business.entity;
+
+import lombok.Data;
+
+@Data
+public class User {
+    String name;
+    String pwd;
+}
